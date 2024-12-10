18ad211a010094046fe958c29bdf4df859a1cf94
Artem's inter on electrolytes <turchina@zeus.nus.edu.sg>
2024-12-10 16:07:33 +0800

    add assert_allclose and divide testing into two functions

--- src/tests/sevennet_equiverance_test.py
+++ src/tests/sevennet_equiverance_test.py
@@ -8,9 +8,9 @@ from src.modules.property_prediction import SevenNetPropertiesPredictor
 from copy import deepcopy
 import torch
 import e3nn.o3 as o3
+from tqdm import tqdm
 
-
-def test_sevennet_equivariance():
+def test_sevennet_equivariance_energies():
 
     dataset = build_dataset()
     checkpoint_name = '7net-0'
@@ -20,7 +20,7 @@ def test_sevennet_equivariance():
     dataloader = DataLoader(dataset, batch_size=batch_size)
 
     
-    for batch in dataloader:
+    for batch in tqdm(dataloader):
         structures = batch.x['atoms']
         initial_atoms_arr = []
         rotated_atoms_arr = []
@@ -51,12 +51,53 @@ def test_sevennet_equivariance():
             initial_atoms_arr.append(atoms)
             rotated_atoms_arr.append(atoms_rotated)
             wigner_matrices_arr.append(wigner_D)
-            
         #energy check
-        assert np.allclose(SevennetPredictor.predict(initial_atoms_arr)['energy'], SevennetPredictor.predict(rotated_atoms_arr)['energy'])
+        np.testing.assert_allclose(SevennetPredictor.predict(initial_atoms_arr)['energy'], SevennetPredictor.predict(rotated_atoms_arr)['energy'], atol=1e-6, rtol=1e-6)
+
+def test_sevennet_equivariance_forces():
+
+    dataset = build_dataset()
+    checkpoint_name = '7net-0'
+    SevennetPredictor = SevenNetPropertiesPredictor(checkpoint_name)
+
+    batch_size = 10
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+
+    
+    for batch in tqdm(dataloader):
+        structures = batch.x['atoms']
+        initial_atoms_arr = []
+        rotated_atoms_arr = []
+        wigner_matrices_arr = []
+
+        for atoms in structures:
+
+            positions = atoms.get_positions() 
+            noise = np.random.normal(loc=0, scale=0.01, size=positions.shape)    
+
+            #add noise
+            atoms.set_positions(positions + noise)
+            atoms_rotated = deepcopy(atoms)
+        
+            positions = atoms_rotated.get_positions() 
+            angles = torch.rand(3) * np.pi * 2
+            wigner_D = o3.wigner_D(1, *angles).detach().numpy()
+
+            fractional_shift = np.random.rand(1, 3)
+            cartesian_shift = np.dot(fractional_shift, atoms.cell)
+
+            #add shift and rotate
+            rotated_positions = (positions + cartesian_shift) @ wigner_D.T
+
+            atoms_rotated.cell = atoms_rotated.cell @ wigner_D.T
+            atoms_rotated.set_positions(rotated_positions)
+
+            initial_atoms_arr.append(atoms)
+            rotated_atoms_arr.append(atoms_rotated)
+            wigner_matrices_arr.append(wigner_D)
 
         #forces check
         for force_initial, force_rotated, wigner_D in zip(SevennetPredictor.predict(initial_atoms_arr)['forces'], SevennetPredictor.predict(rotated_atoms_arr)['forces'], wigner_matrices_arr):
-            assert np.allclose(force_initial @ wigner_D.T, force_rotated, atol=1e-3, rtol=1e-6)
+            np.testing.assert_allclose(force_initial @ wigner_D.T, force_rotated, atol=1e-3, rtol=1e-5)
 
         print('all assertions passed')
