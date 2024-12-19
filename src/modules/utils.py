9f724da82c5d6c026d966ac87f0ae442abaaf14c
Artem Maevskiy <amaevskij@gmail.com>
2024-12-19 15:23:07 +0800

    simiple update to check actions cache (fix style in src/modules/utils.py)

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -8,26 +8,34 @@ from tqdm import tqdm
 CACHE_ENV_VAR = "PFP_CACHE"
 DEFAULT_CACHE_PATH = "./cache"
 
+
 @lru_cache
 def setup_cache(
     cache_path: Optional[str] = None,
 ) -> joblib.Memory:
     if cache_path is None:
         cache_path = os.getenv(CACHE_ENV_VAR, DEFAULT_CACHE_PATH)
-        print(f"Cache path set to \"{cache_path}\". To change, set {CACHE_ENV_VAR} environment variable.")
+        print(
+            f'Cache path set to "{cache_path}". To change, set {CACHE_ENV_VAR} environment variable.'
+        )
 
     return joblib.Memory(cache_path)
 
+
 _memory = setup_cache()
+
 
 @_memory.cache
 def _query_mpid_structure(mpids):
     with open(os.path.abspath(".mp_apikey")) as f:
         MP_API_KEY = f.read().strip()
-        
+
     with MPRester(MP_API_KEY) as mpr:
-        docs = mpr.materials.summary.search(material_ids=mpids, fields=["structure", "material_id"])
+        docs = mpr.materials.summary.search(
+            material_ids=mpids, fields=["structure", "material_id"]
+        )
     return [d.model_dump() for d in docs]
+
 
 def query_mpid_structure(mpids: Union[List[str], str]) -> List[dict]:
     if isinstance(mpids, str):
@@ -35,7 +43,5 @@ def query_mpid_structure(mpids: Union[List[str], str]) -> List[dict]:
 
     return sorted(
         _query_mpid_structure(sorted(mpids)),
-        key=lambda doc: int(doc["material_id"][3:])
+        key=lambda doc: int(doc["material_id"][3:]),
     )
-
-
