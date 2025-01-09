fix bugs: devcontainer, sevennet batch processing, softmax indexing, delete pool_nodes and fix baseline

src/modules/dataset.py
@@ -22,7 +22,6 @@ from pymatgen.io.ase import AseAtomsAdaptor
 # First-party imports
 from modules.utils import query_mpid_structure
 
-
 def build_dataloader_cv(config):
     """
     Build dataloaders for cross-validation.
@@ -133,13 +132,16 @@ class AtomsToGraphCollater(Collater):
         """
         noises = []
         for atoms in batch:
-            positions = atoms.get_positions()
+            new_atoms = deepcopy(atoms)
+            positions = new_atoms.get_positions()
             noise = np.random.normal(loc=0, scale=self.noise_std, size=positions.shape)
-            atoms.set_positions(positions + noise)
-            noises.append(noise)
+            new_atoms.set_positions(positions + noise)
+            noises.append(new_atoms)
         return batch, noises
 
-    def set_noise_to_structures_agg(self, batch: List[Any], num_noisy_configurations: int) -> Any:
+    def set_noise_to_structures_agg(
+        self, batch: List[Any], num_noisy_configurations: int
+    ) -> Any:
         """
         Add noise to atomic structures for aggregation.
 
@@ -165,7 +167,13 @@ class AtomsToGraphCollater(Collater):
         return batch, noises
 
     def transit(
-        self, mass, atoms_batch, noise_structures_batch, forces_batch, energy_batch, log_diffusion
+        self,
+        masses_batch,
+        atoms_batch,
+        noise_structures_batch,
+        forces_batch,
+        energy_batch,
+        log_diffusion_batch,
     ) -> Any:
         """
         Convert noisy structures to graph data.
@@ -180,20 +188,31 @@ class AtomsToGraphCollater(Collater):
             list: List of graph data objects.
         """
         atoms_list = []
-        mass = (
-            torch.from_numpy(mass)
-            .to(forces_batch[0].device)
-            .type(forces_batch[0].dtype)
-        )
 
-        for atoms, noise_structures, forces, energy in zip(
-            atoms_batch, noise_structures_batch, forces_batch, energy_batch
+        for mass, atoms, noise_structures, forces, energy, log_diffusion in zip(
+            masses_batch,
+            atoms_batch,
+            noise_structures_batch,
+            forces_batch,
+            energy_batch,
+            log_diffusion_batch,
+            strict=True,
         ):
-            
+            mass = (
+                torch.from_numpy(mass)
+                .to(forces_batch[0].device)
+                .type(forces_batch[0].dtype)
+            )
+
             if self.forces_divided_by_mass:
                 forces_divided_by_mass = forces / mass[:, None]
-                forces_divided_by_mass_mag = forces_divided_by_mass.norm(dim=1, keepdim=True) 
-                factor = torch.log(1.0 + 1000.0 * forces_divided_by_mass_mag) / forces_divided_by_mass_mag 
+                forces_divided_by_mass_mag = forces_divided_by_mass.norm(
+                    dim=1, keepdim=True
+                )
+                factor = (
+                    torch.log(1.0 + 1000.0 * forces_divided_by_mass_mag)
+                    / forces_divided_by_mass_mag
+                )
                 value = factor * forces_divided_by_mass
             else:
                 forces_mag = forces.norm(dim=1, keepdim=True)
@@ -242,27 +261,50 @@ class AtomsToGraphCollater(Collater):
         Returns:
             list: List of processed graph data.
         """
-        atoms_batch = [data.x["atoms"] for data in batch]
-        data_list = []
+
+        masses_batch = []
+        log_diffusion_batch = []
+        atoms_batch = []
         num_atoms = []
 
         for data in batch:
+            masses_batch.extend(
+                self.num_noisy_configurations * [data.x["atoms"].get_masses()]
+            )
+            log_diffusion_batch.extend(
+                self.num_noisy_configurations * [data.x["log_diffusion"]]
+            )
+            atoms_batch.extend(self.num_noisy_configurations * [data.x["atoms"]])
             num_atoms.append(len(data.x["atoms"]))
-            atoms_batch = [data.x["atoms"] for _ in range(self.num_noisy_configurations)]
-            atoms_batch, noise_structures_batch = self.set_noise_to_structures_agg(
-                deepcopy(atoms_batch), self.num_noisy_configurations
-            )
 
+        atoms_batch, noise_structures_batch = self.set_noise_to_structures(
+            deepcopy(atoms_batch)
+        )
+
+        with torch.enable_grad():
             properites = self.properties_predictor.predict(noise_structures_batch)
-            properties_equilibrium_structure = self.properties_predictor.predict(atoms_batch)
 
-            forces_batch = properites["forces"]
-            energy_batch = [energy_noise - energy_equilibrium for energy_noise, energy_equilibrium in zip(properites["energy"], properties_equilibrium_structure["energy"])]
-            mass = data.x["atoms"].get_masses()
-            log_diffusion = data.x["log_diffusion"]
+        forces_batch = properites["forces"]
+        energy_batch = properites["energy"]
 
-            graphs_batch = self.transit(
-                mass, atoms_batch, noise_structures_batch, forces_batch, energy_batch, log_diffusion
-            )
-            data_list.extend(graphs_batch)
-        return super().__call__(data_list), num_atoms
+        if self.use_energies:
+            energy_equilibrium_batch = self.properties_predictor.predict(atoms_batch)[
+                "energy"
+            ]
+            energy_batch = [
+                energy - energy_equilibrium
+                for energy, energy_equilibrium in zip(
+                    energy_batch, energy_equilibrium_batch, strict=True
+                )
+            ]
+
+        graphs_batch = self.transit(
+            masses_batch,
+            atoms_batch,
+            noise_structures_batch,
+            forces_batch,
+            energy_batch,
+            log_diffusion_batch,
+        )
+
+        return super().__call__(graphs_batch), num_atoms

src/modules/property_prediction.py
@@ -57,7 +57,7 @@ class SevenNetPropertiesPredictor:
         predict: Makes predictions for forces and energy based on a batch of atomic structures.
     """
 
-    def __init__(self, config_name: str, device: str) -> None:
+    def __init__(self, config_name: str, batch_size: int, device: str) -> None:
         """
         Initializes the `SevenNetPropertiesPredictor` with the specified configuration name
         and device.
@@ -69,6 +69,7 @@ class SevenNetPropertiesPredictor:
         checkpoint = sevenn.util.pretrained_name_to_path(config_name)
         sevennet_model, sevennet_config = sevenn.util.model_from_checkpoint(checkpoint)
 
+        self.batch_size = batch_size
         self.device = device
         self.sevennet_model = sevennet_model
         self.sevennet_config = sevennet_config
@@ -88,10 +89,10 @@ class SevenNetPropertiesPredictor:
         """
         self.sevennet_model = self.sevennet_model.to(self.device)
         atoms_list = []
-        atoms_len = []
+        num_atoms_per_structure = []
         for atoms in batch:
             atoms_list.append(assign_dummy_y(atoms))
-            atoms_len.append(atoms.get_positions().shape[0])
+            num_atoms_per_structure.append(atoms.get_positions().shape[0])
 
         atoms_list = _set_atoms_y(atoms_list)
 
@@ -109,30 +110,24 @@ class SevenNetPropertiesPredictor:
         sevennet_inference_set.toggle_requires_grad_of_data(sevenn._keys.POS, True)
         sevennet_infer_list = sevennet_inference_set.to_list()
 
-        sevennet_batch = DataLoader(
-            sevennet_infer_list, batch_size=len(sevennet_infer_list), shuffle=False
+
+        sevennet_data = DataLoader(
+            sevennet_infer_list, batch_size=self.batch_size, shuffle=False
         )
-
-        (sevennet_batch,) = sevennet_batch
-        sevennet_batch = sevennet_batch.to(self.device)
-
-        with torch.enable_grad():
-            sevennet_output = self.sevennet_model(sevennet_batch).detach()
 
         forces = []
         energies = []
-        total_lenn = 0
 
-        for index, lenn in enumerate(atoms_len):
-            forces.append(
-                sevennet_output.inferred_force[total_lenn : total_lenn + lenn, :]
-                .clone()
-                .detach()
-            )
-            energies.append(
-                sevennet_output.inferred_total_energy[index].clone().detach()
-            )
-            total_lenn += lenn
+        for sevennet_batch in sevennet_data:
+            sevennet_batch = sevennet_batch.to(self.device)
+            sevennet_output = self.sevennet_model(sevennet_batch)
+            forces.append(sevennet_output.inferred_force.detach())
+            energies.append(sevennet_output.inferred_total_energy.detach())
+
+        energies = list(torch.split(torch.cat(energies), 1))
+        forces = list(torch.split(torch.cat(forces), num_atoms_per_structure, dim=0))
+        assert len(forces) == len(energies) == len(num_atoms_per_structure)
+        assert type(forces) == type(energies) == type([])
 
         return {
             "forces": forces,

src/modules/train.py
@@ -4,11 +4,9 @@ Handles data loading, processing, training, and evaluation.
 """
 
 import os
-import numpy as np
 import torch
 import torch_scatter
 from torch import nn, optim
-import torch.nn.functional as F
 from sklearn.metrics import r2_score, mean_squared_error
 from tqdm import tqdm
 import wandb
@@ -45,22 +43,22 @@ class Trainer:
             "device", "cuda" if torch.cuda.is_available() else "cpu"
         )
         self.model = self.model.to(self.device)
-        self.pool_nodes = config["predict_importance"]["pool_nodes"]
         self.criterion = self._get_criterion(self.training_config["criterion"])
         self.optimizer = self._get_optimizer()
 
         self.num_epochs = self.training_config["num_epochs"]
         self.num_noisy_configurations = self.training_config["num_noisy_configurations"]
 
-        self.softmax_within_single_structure_by_configurations = self.config["predict_importance"][
-            "softmax_within_single_structure_by_configurations"
-        ]
-        self.softmax_within_single_atom_by_configurations = self.config["predict_importance"][
+        self.softmax_within_single_structure_by_configurations = self.config[
+            "training"
+        ]["softmax_within_single_structure_by_configurations"]
+        self.softmax_within_single_atom_by_configurations = self.config["training"][
             "softmax_within_single_atom_by_configurations"
         ]
-        self.softmax_within_configurations = self.config["predict_importance"][
+        self.softmax_within_configurations = self.config["training"][
             "softmax_within_configurations"
         ]
+        self.predict_importance = self.config["model"]["predict_importance"]
 
         if config["wandb"]["verbose"]:
             tags = []
@@ -70,15 +68,15 @@ class Trainer:
                 tags.append("use_displacements")
             if self.training_config["use_energies"]:
                 tags.append("use_energies")
-            if self.config["predict_importance"]["pool_nodes"]:
-                tags.append("pool_nodes")
-            if self.config["predict_importance"]["softmax_within_single_structure_by_configurations"]:
+            if self.config["training"][
+                "softmax_within_single_structure_by_configurations"
+            ]:
                 tags.append("softmax_within_single_structure_by_configurations")
-            if self.config["predict_importance"]["softmax_within_single_atom_by_configurations"]:
+            if self.config["training"]["softmax_within_single_atom_by_configurations"]:
                 tags.append("softmax_within_single_atom_by_configurations")
-            if self.config["predict_importance"]["softmax_within_configurations"]:
+            if self.config["training"]["softmax_within_configurations"]:
                 tags.append("softmax_within_configurations")
-                                    
+
             wandb.init(
                 entity=config["wandb"]["entity_name"],
                 project=config["wandb"]["project_name"],
@@ -91,7 +89,7 @@ class Trainer:
         """Return the appropriate loss criterion."""
         if name == "MSELoss":
             return nn.MSELoss()
-        raise ValueError(f"Unsupported criterion: {name}")
+        raise NotImplementedError(f"Unsupported criterion: {name}")
 
     def _get_optimizer(self):
         """Return the appropriate optimizer."""
@@ -103,7 +101,7 @@ class Trainer:
             return optim.Adam(
                 self.model.parameters(), lr=learning_rate, weight_decay=weight_decay
             )
-        raise ValueError(f"Unsupported optimizer: {optimizer_name}")
+        raise NotImplementedError(f"Unsupported optimizer: {optimizer_name}")
 
     def _thorough_validation(self, dataloader):
         y_pred_agg = []
@@ -112,22 +110,20 @@ class Trainer:
 
             with torch.set_grad_enabled(False):
                 for data, num_atoms in tqdm(dataloader):
-                    batch_loss, batch_y_true, batch_y_pred, batch_entropy = (
-                        self._process_batch(data, num_atoms, train=train)
+                    _, _, batch_y_pred, _ = self._process_batch(
+                        data, num_atoms, train=False
                     )
-                    total_loss += batch_loss
-                    num_samples += len(num_atoms)
                     preds.extend(batch_y_pred)
             y_pred_agg.append(preds)
 
         y_pred = (
-            torch.stack([torch.concatenate(p) for p in y_pred_agg], dim=0)
+            torch.stack([torch.stack(p) for p in y_pred_agg], dim=0)
             .mean(axis=0)
             .cpu()
             .detach()
             .numpy()
         )
-        
+
         y_true = (
             torch.concatenate([i.target.cpu() for i, _ in dataloader])
             .cpu()
@@ -140,6 +136,54 @@ class Trainer:
 
         return val_thorough_loss, val_thorough_r2
 
+    def _generate_index_arrays(self, num_atoms):
+
+        indexes = torch.arange(len(num_atoms) * self.num_noisy_configurations)
+        repeats = torch.tensor(num_atoms).repeat_interleave(
+            self.num_noisy_configurations
+        )
+        indexing_noise_variations = indexes.repeat_interleave(repeats).to(self.device)
+
+        indexing_atoms = []
+        last_idx = 0
+        for num in num_atoms:
+            indexing_atoms.append(
+                torch.arange(last_idx, last_idx + num).repeat(
+                    self.num_noisy_configurations
+                )
+            )
+            last_idx += num
+        indexing_atoms = torch.cat(indexing_atoms).to(self.device)
+
+        indexes = torch.arange(len(num_atoms))
+        repeats = torch.tensor(num_atoms) * self.num_noisy_configurations
+        indexing_both = indexes.repeat_interleave(repeats).to(self.device)
+
+        return indexing_atoms, indexing_noise_variations, indexing_both
+
+    def _choose_index(self, indexing_atoms, indexing_noise_variations, indexing_both):
+        if self.softmax_within_single_atom_by_configurations:
+            return indexing_atoms
+        if self.softmax_within_single_structure_by_configurations:
+            return indexing_noise_variations
+        if self.softmax_within_configurations:
+            return indexing_both
+        raise NotImplementedError("Unknown type of aggregation")
+
+    def _calculate_entropy(self, importances, aggregation_index):
+        entropy = torch_scatter.scatter_sum(
+            -importances * torch.log(importances), aggregation_index, dim=0
+        )
+        normalization = torch.log(
+            torch_scatter.scatter_sum(
+                torch.ones(importances.shape, device=self.device),
+                aggregation_index,
+                dim=0,
+            )
+        )
+        entropy /= normalization
+        return entropy
+
     def _process_batch(self, data, num_atoms, train=True):
         """Process a single batch of data."""
         if train:
@@ -147,111 +191,55 @@ class Trainer:
 
         data = data.to(self.device)
         outputs = self.model(data)
-        predictions, importances = (
-            outputs[..., 0],
-            outputs[..., 1],
-        )
+
+        if self.predict_importance:
+            predictions, importances = (
+                outputs[..., 0],
+                outputs[..., 1],
+            )
+        else:
+            predictions = outputs
+
         loss = 0.0
         y_true, y_pred = [], []
 
-        if self.softmax_within_single_structure_by_configurations:
-            indexes = torch.arange(len(num_atoms) * self.num_noisy_configurations)
-            repeats = torch.tensor(num_atoms).repeat_interleave(self.num_noisy_configurations)
-            idx = indexes.repeat_interleave(repeats).to(self.device)
+        indexing_atoms, indexing_noise_variations, indexing_both = (
+            self._generate_index_arrays(num_atoms)
+        )
+        aggregation_index = self._choose_index(
+            indexing_atoms, indexing_noise_variations, indexing_both
+        )
 
-            importances = torch_scatter.scatter_softmax(importances, idx, dim=0)
-            entropy = torch_scatter.scatter_sum(
-                -importances * torch.log(importances), idx, dim=0
+        if self.predict_importance:
+            importances = torch_scatter.scatter_softmax(
+                importances, aggregation_index, dim=0
             )
-            normalization = (
-                torch.log(torch.tensor(num_atoms))
-                .repeat_interleave(self.num_noisy_configurations)
-                .to(self.device)
+            entropy = self._calculate_entropy(importances, aggregation_index)
+        else:
+            entropy = -torch.ones(len(num_atoms))
+
+        if self.predict_importance:
+            intermediate_predictions = torch_scatter.scatter_sum(
+                importances * predictions, aggregation_index, dim=0
             )
-            entropy /= normalization
-            assert entropy.shape[0] == self.num_noisy_configurations * len(data)
+        else:
             intermediate_predictions = torch_scatter.scatter_mean(
-                importances * predictions, idx, dim=0
+                predictions, aggregation_index, dim=0
             )
-            idx_agg = (
-                torch.arange(len(num_atoms))
-                .repeat_interleave(self.num_noisy_configurations)
-                .to(self.device)
-            )
-            final_predictions = torch_scatter.scatter_mean(
-                intermediate_predictions, idx_agg, dim=0
-            )
+        idx_1, _ = torch_scatter.scatter_min(indexing_both, aggregation_index, dim=0)
+        idx_2, _ = torch_scatter.scatter_max(indexing_both, aggregation_index, dim=0)
 
-        if self.softmax_within_single_atom_by_configurations:
-            idx = []
-            last_idx = 0
-            for num in num_atoms:
-                idx.append(
-                    torch.arange(last_idx, last_idx + num).repeat(self.num_noisy_configurations)
-                )
-                last_idx += num
-            idx = torch.cat(idx).to(self.device)
+        assert torch.equal(idx_1, idx_2)
 
-            importances = torch_scatter.scatter_softmax(importances, idx, dim=0)
-            entropy = torch_scatter.scatter_sum(
-                -importances * torch.log(importances), idx, dim=0
-            )
-            normalization = torch.log(torch.tensor(self.num_noisy_configurations, dtype = torch.float32))
-            entropy /= normalization
-            assert entropy.shape[0] == sum(num_atoms)
-            intermediate_predictions = torch_scatter.scatter_mean(
-                importances * predictions, idx, dim=0
-            )
+        final_predictions = torch_scatter.scatter_mean(
+            intermediate_predictions, idx_1, dim=0
+        )
 
-            idx_atoms = (
-                torch.arange(len(num_atoms))
-                .repeat_interleave(torch.tensor(num_atoms))
-                .to(self.device)
-            )
-            final_predictions = torch_scatter.scatter_mean(
-                intermediate_predictions, idx_atoms, dim=0
-            )
-
-        if self.softmax_within_configurations:
-            indexes = torch.arange(len(num_atoms))
-            repeats = torch.tensor(num_atoms) * self.num_noisy_configurations
-            idx = indexes.repeat_interleave(repeats).to(self.device)
-
-            importances = torch_scatter.scatter_softmax(importances, idx, dim=0)
-
-            entropy = torch_scatter.scatter_sum(
-                -importances * torch.log(importances), idx, dim=0
-            )
-            normalization = torch.log(torch.tensor(num_atoms) * self.num_noisy_configurations).to(
-                self.device
-            )
-            entropy /= normalization
-            assert entropy.shape[0] == len(num_atoms)
-            final_predictions = torch_scatter.scatter_mean(
-                importances * predictions, idx, dim=0
-            )
-
-        if self.pool_nodes:
-            idx = (
-                torch.arange(len(num_atoms))
-                .repeat_interleave(self.num_noisy_configurations)
-                .to(self.device)
-            )
-            importances = torch_scatter.scatter_softmax(importances, idx, dim=0)
-            entropy = torch_scatter.scatter_sum(
-                -importances * torch.log(importances), idx, dim=0
-            )
-
-            normalization = torch.log(torch.tensor(self.num_noisy_configurations, dtype = torch.float32))
-
-            final_predictions = torch_scatter.scatter_mean(
-                importances * predictions, idx, dim=0
-            )
-
-            assert entropy.shape[0] == len(data)
-            
         y_true = data["target"][:: self.num_noisy_configurations]
         y_pred = final_predictions.detach()
+
+        if not self.predict_importance:
+            final_predictions = final_predictions.squeeze()
 
         loss += self.criterion(final_predictions, y_true)
 
@@ -288,7 +276,9 @@ class Trainer:
         r2 = r2_score(y_true, y_pred)
         avg_loss = total_loss / num_samples
 
-        return avg_loss, r2, entropy.mean()
+        mean_entropy = entropy.mean()
+
+        return avg_loss, r2, mean_entropy
 
     def train_epoch(self):
         """Train for one epoch."""
@@ -318,10 +308,16 @@ class Trainer:
                         "val_loss": avg_val_loss,
                         "r2_train": r2_train,
                         "r2_val": r2_val,
-                        "entropy_train": entropy_train,
-                        "entropy_train": entropy_val,
                     }
                 )
+
+                if self.predict_importance:
+                    wandb.log(
+                        {
+                            "entropy_train": entropy_train,
+                            "entropy_val": entropy_val,
+                        }
+                    )
 
                 if (
                     self.config["training"]["num_noisy_configurations"] == 1
@@ -331,6 +327,7 @@ class Trainer:
                     thorough_train_loss, thorough_train_r2 = self._thorough_validation(
                         self.train_dataloader
                     )
+
                     thorough_val_loss, thorough_val_r2 = self._thorough_validation(
                         self.val_dataloader
                     )